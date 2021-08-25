512507cb956ff1ff33adf7d0283d8c9725ecb0a7
mariklim <maria.klimenko889@gmail.com>
2021-08-25 16:02:39 +0200

    creato array vuoto e ciclo per chiedere i numeri

---
+++ snack-5/script.js
@@ -0,0 +1,10 @@
+// 1. creo ciclo per chiedere 6 volte il nemero all'utente
+// 2. creo array vuoto per i numeri
+// 3.  se sono dispari inserisco nell’array
+
+var listaNumeri = [];
+
+    for (var i = 0; i < 6; i++){
+    var numero = parseInt(prompt("Inserisci un numero"));
+    console.log(numero);
+} 
